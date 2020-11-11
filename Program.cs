039d8f9bd20d568f580f879f9bc9073e5d6ad519
richard <richard_lucas.om12@gmail.com>
2020-11-11 16:26:53 -0300

    projeto-c#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace projeto_C_
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Digite um numero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro numero");
+            int num2 = int.Parse(Console.ReadLine());
+            int result = num1 + num2;
+            Console.WriteLine("resultado = "+ result + " seu burro");
+        }
+    }
+}
